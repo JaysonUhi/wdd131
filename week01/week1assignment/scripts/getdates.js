45ecd3c751526bea454061e5f034b3ccc839c356
JaysonUhi <jaysonuhi@gmail.com>
2024-11-09 23:43:05 -0700

    pageweek1

---
+++ week01/week1assignment/scripts/getdates.js
@@ -0,0 +1,7 @@
+// getdates.js
+
+// Dynamically set the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Dynamically set the last modified date
+document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
